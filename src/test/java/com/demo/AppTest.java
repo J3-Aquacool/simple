0e31a31be870af159d698e6a03bc347b2b97001d
J3-Aquacool <jagdish.java2@gmail.com>
2023-10-28 17:19:15 +0530

    added project

---
+++ src/test/java/com/demo/AppTest.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+	@Test
+	void test() {
+		System.out.println("Runnig test  cases");
+		assertTrue(true);
+		
+	}
+}
